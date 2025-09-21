671166c0c6344eb68dd5172c9acac25e1948cad7
umcap-pwn <yakovprud@gmail.com>
2025-09-21 11:02:30 +0700

    THIS SHIT IS ALIVE

--- dvm.c
+++ dvm.c
@@ -1,56 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib/stack.h"
-#include <stdint.h>
+#include "lib/dcore.h"
+#include "lib/misc.h"
+#include "lib/dops.c"
 
 /* Virtual mashine for executing .dcode sequenses. */
 
-/* Used to bundle operator and operand together. More readable than the embedded operands variant, though may waste some memory. */
-typedef struct {
-	uint8_t  *opcode;
-	int          arg;	
-} instruction;
-
-/* General VM state. Memory management implemented primarily through a stack. */
-typedef struct {
-	int                *sp;
-	int                *ip;
-	instruction      *prog;
-	int         stack[256];
-} VM;
-
-/* Just some helpful .dcode decls. Implementation can be found in lib/dops.h */
+/* Just some helpful .dcode decls. Implementation can be found in lib/dops.c */
 extern int _PUSH  (VM *vm);
 extern int _ADD   (VM *vm);
 extern int _PRINT (VM *vm);
+extern int _RET   (VM *vm);
 
 /* Basic auxiliary function set. Decls in lib/misc.h */
-extern int step     (VM *vm, instruction *seq);
+extern int step     (VM *vm, int (*ops[])(VM*));
 extern int parse_bc (char* file, instruction *seq);
-extern int exec     (int (*op)(VM *vm));
+//extern int exec     (VM *vm, int (*op)(VM *vm));
 
 /* Initialisation of core data structs. */
-int (*ops[])(VM *) = {
-	_PUSH,
-	_ADD, 
-	_PRINT
-};
-instruction seq[1024];
+extern int (*ops[])(VM*);   // defined in lib/dops.c
+instruction seq[1024];      // placed on .bss 
 
 
 int main (int argc, char **argv){
 
-	parse_bc(argv[1], seq);
+	//parse_bc(argv[1], seq);
+
+	for(uint8_t i=0;i<4;i++){
+		instruction o = {i, 0};
+		seq[i] = o;
+	}
 
 	VM vm = {
 		.sp = 0,
 		.ip = 0,
-		.prog = seq 
+		.prog = seq //TODO: insecure transition. len(seq) is need to be stored somwhere 
 	};
 
 	for(;;)
 	{
-		if (step(&vm, seq)) break;
+		if (step(&vm, ops)) break; // main VM lifecycle. TODO: error handling, stack trace. 
 	}
-	
 }
